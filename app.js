5b1a952a18090046d62c7cdb52baa950f009792c
info <58048398+kaushikappani@users.noreply.github.com>
2021-04-10 14:23:23 +0530

    new

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+
 
 const app = express();
 
 const axios = require('axios');
-const { Console } = require('console');
 
 
 
@@ -19,27 +18,9 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost:27017/helloworld', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-mongoose.connection
-    .once('open', () => {
-        console.log(`connected to database`)
-    })
-    .on('error', (err) => {
-        console.log(`error: ${err}`)
-    });
-mongoose.set('useFindAndModify', false);
 
 app.set("view engine", "ejs");
 
-
-app.get('/json', (req, res) => {
-    University.find({}).then((data) => {
-        res.send(data);
-    })
-});
 
 
 
@@ -70,16 +51,6 @@ app.get('/predictor', (req, res) => {
     }
     res.render('predictor', {
         chance:chance
-    })
-})
-
-app.get('/search/:name', (req, res) => {
-    University.find({ Name: req.params.name }, (err, data) => {
-        if (!err) {
-            res.send(data)
-        } else {
-            console.log(err)
-        }
     })
 })
 
